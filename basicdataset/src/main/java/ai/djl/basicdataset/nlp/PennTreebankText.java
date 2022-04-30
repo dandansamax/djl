dfa5906fbf68ec8019ef4740c253ea3ffcc0a77c
Akagiwyf <11912913@mail.sustech.edu.cn>
2022-04-30 22:32:21 +0800

    improve the test method and make it simpler

--- basicdataset/src/main/java/ai/djl/basicdataset/nlp/PennTreebankText.java
+++ basicdataset/src/main/java/ai/djl/basicdataset/nlp/PennTreebankText.java
@@ -31,7 +31,7 @@ import java.util.List;
 
 /**
  * The Penn Treebank (PTB) project selected 2,499 stories from a three year Wall Street Journal
- * (WSJ) collection of 98,732 stories for syntactic annotation. see <a *
+ * (WSJ) collection of 98,732 stories for syntactic annotation. see <a
  * href="https://catalog.ldc.upenn.edu/docs/LDC95T7/cl93.html">here</a> for details
  */
 public class PennTreebankText extends TextDataset {
